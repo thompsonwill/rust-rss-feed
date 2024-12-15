ce10b3ad3ee7d46c24a59c38d40374f41a35fbad
Will Thompson <williamjamesthompson@gmail.com>
2024-12-15 11:05:47 -0600

    This is real ugly I should preobably start over

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,14 +1,19 @@
 import { invoke } from "@tauri-apps/api/core";
 import { useEffect, useState } from "react";
+import { House, LayoutDashboard } from "lucide-react";
+
+import { NavLink } from "react-router";
+
 import "./landing.css";
 function Landing() {
   const [feed, setFeed] = useState(null);
   const [loading, setLoading] = useState(true);
+  const [active, setActive] = useState("home");
 
   async function getRssFeed() {
     try {
       const feedData = await invoke("fetch_rss_feed", {
-        url: "https://techcrunch.com/feed/",
+        url: "https://feeds.simplecast.com/Ao0C24M8",
       });
       console.log("Feed Title:", feedData.title);
       setFeed(feedData);
@@ -26,7 +31,46 @@ function Landing() {
   }, []);
 
   return (
-    <div>
+    <div className="landing-page-container">
+      <div className="feed-sidebar">
+        <div className="feed-menu-title">
+          <p>Your Podcasts</p>
+        </div>
+        <NavLink to="/" end>
+          <div
+            className={`feed-sidebar-item ${
+              active == "/" ? " active-feed-sidebar" : ""
+            }`}
+            onClick={() => setActive("home")}
+          >
+            <div className="feed-sidebar-item-img-name">
+              <img
+                src="https://image.simplecastcdn.com/images/00c81e60-45f9-4643-9fed-2184b2b6a3d3/9fb18d59-cb0c-4d92-ba6b-ee54eca6d2ea/3000x3000/earwolf-sxm-hdtgm.jpg?aid=rss_feed"
+                style={{ height: "20px", width: "20px" }}
+              />
+              <p>Podcast #1</p>
+            </div>
+            {/* <p style={{ fontSize: "10px" }}>Latest: 12/14/24</p> */}
+          </div>
+        </NavLink>
+        <NavLink to="/" end>
+          <div
+            className={`feed-sidebar-item ${
+              active == "/" ? " active-feed-sidebar" : ""
+            }`}
+            onClick={() => setActive("home")}
+          >
+            <div className="feed-sidebar-item-img-name">
+              <img
+                src="https://image.simplecastcdn.com/images/00c81e60-45f9-4643-9fed-2184b2b6a3d3/9fb18d59-cb0c-4d92-ba6b-ee54eca6d2ea/3000x3000/earwolf-sxm-hdtgm.jpg?aid=rss_feed"
+                style={{ height: "20px", width: "20px" }}
+              />
+              <p>Podcast #1</p>
+            </div>
+            {/* <p style={{ fontSize: "10px" }}>Latest: 12/14/24</p> */}
+          </div>
+        </NavLink>
+      </div>
       {!loading && feed ? (
         <div>
           <div className="feed-source-title">
@@ -47,10 +91,11 @@ function Landing() {
               return (
                 <div>
                   <h3>{item.title}</h3>
-                  <div
+                  <p>{item.pub_date}</p>
+                  {/* <div
                     dangerouslySetInnerHTML={{ __html: item.description }}
-                  ></div>
-                  <button>read more</button>
+                  ></div> */}
+                  {/* <button>read more</button> */}
                 </div>
               );
             })}
